c8414ec2f9f5f04be856d053e193e03f86847ff6
Lucian Nguyen <lucian.n@seznam.cz>
2022-03-16 19:46:32 +0100

    all migrations repaired and gathered together

--- database/migrations/2022_03_15_165456_create_food_table.php
+++ database/migrations/2022_03_15_165456_create_food_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('food', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->unique();
             $table->string('address')->nullable();
             $table->string('name')->nullable();
             $table->string('description')->nullable();
--- database/migrations/2022_03_15_165457_create_users_table.php
+++ database/migrations/2022_03_15_165457_create_users_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();;
+            $table->string('name')->nullable();
             $table->string('login')->unique();
             $table->string('email')->unique();
-            $table->string('password')->nullable();;
-            $table->string('address')->nullable();;
-            $table->string('contacts')->nullable();;
+            $table->string('password')->nullable();
+            $table->string('address')->nullable();
+            $table->string('contacts')->nullable();
             $table->integer('phone_number')->unique();
             $table->timestamps();
         });
