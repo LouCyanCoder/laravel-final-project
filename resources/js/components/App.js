b9af36d2dcf19ec1db9d30abac481685ee01c060
Gheorghi Ivanciuc <grishan4ik@hotmail.com>
2022-03-22 11:29:40 +0100

    commit before pull (map_start)

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Landing from "./Landing";
-import Features from "./Features"
-import Contact from "./Contact"
-import AboutUs from "./AboutUs"
+import Features from "./Features";
+import Contact from "./Contact";
+import AboutUs from "./AboutUs";
+import Map from "./Map";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 
 const App = () => {
     return <div>
+
         <Landing />
-        
+
+        <div className="mapp"><Map /></div>
+
         <Features />
 
         <Contact />
