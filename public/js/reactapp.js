c91e6241025998cd9b5f33174dc6e3bb97a77192
Lucian Nguyen <lucian.n@seznam.cz>
2022-03-28 15:49:19 +0200

    commit before merging the main

--- public/js/reactapp.js
+++ public/js/reactapp.js
@@ -24609,7 +24609,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _FoodList__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./FoodList */ "./resources/js/components/Dashboard/FoodList.js");
 /* harmony import */ var _TransportList__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./TransportList */ "./resources/js/components/Dashboard/TransportList.js");
 /* harmony import */ var _ServiceList__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./ServiceList */ "./resources/js/components/Dashboard/ServiceList.js");
-Object(function webpackMissingModule() { var e = new Error("Cannot find module '../MapComponents'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _MapComponents__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../MapComponents */ "./resources/js/components/MapComponents.js");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 
 
@@ -24801,7 +24801,7 @@ var OffersListed = function OffersListed() {
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__.jsxs)("div", {
       children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__.jsx)("h3", {
         children: "Accommodation"
-      }), selected_accommodation.length ? /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__.jsx)(Object(function webpackMissingModule() { var e = new Error("Cannot find module '../MapComponents'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()), {
+      }), selected_accommodation.length ? /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__.jsx)(_MapComponents__WEBPACK_IMPORTED_MODULE_6__["default"], {
         Component: _AccommodationList__WEBPACK_IMPORTED_MODULE_2__["default"],
         arry: selected_accommodation
       }) : /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_7__.jsx)("p", {
@@ -26013,6 +26013,38 @@ function Map(_ref) {
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Map);
+
+/***/ }),
+
+/***/ "./resources/js/components/MapComponents.js":
+/*!**************************************************!*\
+  !*** ./resources/js/components/MapComponents.js ***!
+  \**************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+
+var MapComponents = function MapComponents(_ref) {
+  var Component = _ref.Component,
+      arry = _ref.arry;
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+    children: arry.map(function (element, index) {
+      return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(Component, {
+        element: element
+      }, index);
+    })
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MapComponents);
 
 /***/ }),
 
